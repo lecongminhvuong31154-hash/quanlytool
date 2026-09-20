befab41cfc8353f539f6aa9615dee244dfd23ebb
lecongminhvuong31154-hash <lecongminhvuong31154@gmail.com>
2026-09-20 22:51:08 +0700

    Add files via upload

--- api-config.js
+++ api-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "quanlytool-backend-quanlytool.up.railway.app",
+  API_BASE_URL: "https://quanlytool-backend-quanlytool.up.railway.app",
   API_TIMEOUT_MS: 8000
 };
